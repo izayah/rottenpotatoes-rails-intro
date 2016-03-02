68df946b5e650ede8b214e0730f1445521d56f71
izayahallen <izayahallen@gmail.com>
2016-03-02 22:00:56 +0000

    added initial sorting fnctn

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,13 +12,13 @@ class MoviesController < ApplicationController
 
   def index
     if params[:sort_by] == "alpha"
-     @title_header = hilite
+     @title_header = 'hilite'
      #@release_date_header = "hilite"
      return @movies = Movie.order(title: :asc)
     end
     if params[:sort_by] == "date"
       #@title_header = "hilite"
-      @release_date_header = 'hilite'
+      @release_date_header = "hilite"
     return @movies = Movie.order(release_date: :asc)
     end
     @movies = Movie.all
