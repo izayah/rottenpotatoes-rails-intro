1b58d9cceff35a2f69efb0e31b5273e955ac0e8a
izayahallen <izayahallen@gmail.com>
2016-03-03 00:28:17 +0000

    working on buttons

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
     @sorting_style = params[:sort_by]
     if @sorting_style == "alpha"
      return @movies = Movie.order(title: :asc)
