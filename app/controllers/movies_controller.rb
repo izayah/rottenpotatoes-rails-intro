d988a985aa6c0937a2cb3d2b656bac49025a9f62
izayahallen <izayahallen@gmail.com>
2016-03-04 21:09:51 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,7 +22,7 @@ class MoviesController < ApplicationController
     
     if @sorting_style == "alpha"
       @title_sort=true
-     return @movies = Movie.where("rating = ?", ratings_filter).order(title: :asc)
+     return @movies = Movie.where("rating = ?", params["ratings"]).order(title: :asc)
     end
    if @sorting_style == "date"
      @release_date_sort=true
