b32605671683c67567ad986c908fc11408acf3f9
izayahallen <izayahallen@gmail.com>
2016-03-04 21:47:20 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
      @release_date_sort=true
    return @movies = Movie.order(release_date: :asc)
    end
-    @movies = Movie.where("rating = ?", params[:ratings][rating] == '1')
+    @movies = Movie.where("rating = ?", params[:ratings] == '1')
   end
   
   def new
