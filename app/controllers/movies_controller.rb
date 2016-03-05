92122ed102612deda3ab65dd41b7bb5191824178
izayahallen <izayahallen@gmail.com>
2016-03-05 04:02:19 +0000

    p3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,36 +12,23 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
     @sorting_style = params[:sort_by]
-   # session[:params] ||= {}
-    old_params = session[:params]
-    
-   # params[:sort_by] = old_params[:sort_by] if params[:sort_by].nil?
-   #params[:ratings] = old_params[:ratings] if params[:ratings].nil?
-    #redirect_to movies_path if({:sort_by => old_params[:sort_by], :ratings => old_params[:ratings]}) if (params[:sort_by].blank? && old[:sort_by].present?) || (params[:ratings].blank? && old_params[:ratings].present?)
-    
      if params[:ratings].nil?
       ratings_filter = @all_ratings
      else
       ratings_filter = params["ratings"].keys
      end
     
-    #if params[:ratings] || params[:sort_by]
-    if @sorting_style == "alpha"
-      @title_sort=true
-      return @movies = Movie.where(:rating => ratings_filter).order(title: :asc)
-    end
-   if @sorting_style == "date"
-     @release_date_sort=true
-   return @movies = Movie.where(:rating => ratings_filter).order(release_date: :asc)
-   end
-  # if session[:ratings] || session[:sort_by]
-     #flash.keep 
-    # redirect_to movies_path :sort_by => session[:sort_by], :ratings => session[:ratings]
-   # @movies = Movie.where(:rating => ratings_filter)
-   #end
-   # end
-  def new
-    # default: render 'new' template
+    #if params.has_key?[:sort_by]
+      if @sorting_style == "alpha"
+        @title_sort=true
+        return @movies = Movie.where(:rating => ratings_filter).order(title: :asc)
+      end
+      if @sorting_style == "date"
+       @release_date_sort=true
+       return @movies = Movie.where(:rating => ratings_filter).order(release_date: :asc)
+      end
+      @movies = Movie.where(:rating => ratings_filter)
+    #end
   end
 
   def create
